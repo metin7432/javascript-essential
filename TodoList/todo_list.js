ba4d20268d4b6990b8d671e5f1b36c128faf3335
metin7432 <metin7432@gmail.com>
2025-03-17 16:06:08 -0400

    Todo list project

---
+++ TodoList/todo_list.js
@@ -0,0 +1,56 @@
+const taskInput = document.getElementById("taskInput");
+const addTaskBtn = document.getElementById("addTaskBtn");
+const taskList = document.getElementById("taskList");
+const clearCompletedBtn = document.getElementById("clearCompletedBtn");
+
+let tasks = [];
+
+function addTask(){
+    // inputtun veri al ve bosluklari temizle
+    const taskText = taskInput.value.trim();
+
+    //eger taskTest bos degilse
+    if(taskText !== ""){
+
+        // tasks arrayine pushla
+        tasks.push({text: taskText});
+
+        // task inptuna bos deger ver
+        taskInput.value = " ";
+
+        // Tasklari goster
+        displayTasks()
+    }
+}
+
+function  displayTasks(){
+// inputa bos deger ata    
+taskList.innerHTML = "";
+    // tasks arrayini forEach'le 
+    tasks.forEach((task, index) => {
+      // li elementi olustur  
+      const li = document.createElement("li");
+      // li elementine veri ekle
+      li.innerHTML = `<input type="checkbox" id="task-${index}" ${task.completed ? "checked" : ""}>
+                    <label for="task-${index}">${task.text}</label>`;
+      //querySelector ile input elementini sec change Evetini ekle
+      li.querySelector("input").addEventListener("change", () => toggleTask(index));
+      //ul elementine child olan li ekleniyor
+      taskList.appendChild(li);
+    })
+}
+
+function toggleTask(index) {
+    tasks[index].completed = !tasks[index].completed;
+    displayTasks();
+}
+
+function clearCompletedTasks() {
+    tasks = tasks.filter(task => !task.completed);
+    displayTasks();
+}
+
+addTaskBtn.addEventListener("click", addTask);
+clearCompletedBtn.addEventListener("click", clearCompletedTasks);
+
+displayTasks();
